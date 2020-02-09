1036868a61dcbc1ac4f5c10215818ae3930d008d
igram <igram.jp@gmail.com>
2020-02-09 16:07:46 +0900

    quick tour

--- src/second.rs
+++ src/second.rs
@@ -1,15 +1,32 @@
-pub fn sort(x: &mut [u32], up: bool) {
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of x is not a power of two. (x.len(): {})",
+            x.len()
+        ))
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     // unimplemented!();
 
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
 
-fn sub_sort(x: &mut [u32], up: bool) {
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     // unimplemented!();
 
     if x.len() > 1 {
@@ -20,7 +37,7 @@ fn sub_sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn compare_and_swap(x: &mut [u32], up: bool) {
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     // unimplemented!();
 
     let mid_point = x.len() / 2;
@@ -36,19 +53,82 @@ fn compare_and_swap(x: &mut [u32], up: bool) {
 mod tests {
     // 親モジュール (first) の sort 関数を使用する
     use super::sort;
+    use crate::SortOrder::*;
 
     // #[test] の付いた関数は cargo test した時に実行される
     #[test]
     fn sort_u32_ascending() {
-        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, true);
-        assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
-        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, false);
-        assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with"
+            ]
+        );
+    }
+
+    #[test]
+    fn sort_str_descending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "with",
+                "no",
+                "memory-efficient",
+                "is",
+                "fast",
+                "and",
+                "Rust",
+                "GC"
+            ]
+        );
+    }
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
     }
 }
